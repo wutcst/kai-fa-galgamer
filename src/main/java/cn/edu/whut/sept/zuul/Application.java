0cdfe3e7ec1c856600423fa52c5360057605fc0f
47ghost <dd2005040302@gmail.com>
2026-06-06 13:35:15 +0800

    init: 后端初始化

---
+++ src/main/java/cn/edu/whut/sept/zuul/Application.java
@@ -0,0 +1,17 @@
+package cn.edu.whut.sept.zuul;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Configurable
+@Slf4j
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+        log.info("服务启动-8080");
+    }
+}
---
+++ src/main/java/cn/edu/whut/sept/zuul/controller/package-info.java
@@ -0,0 +1,2 @@
+package cn.edu.whut.sept.zuul.controller;
+//controller层
